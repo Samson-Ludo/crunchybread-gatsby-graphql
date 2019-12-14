c04d193ac5e8d9880bb7d3348186859f2342d8d2
Samson Okaludo <43629180+Samson-Ludo@users.noreply.github.com>
2019-12-14 15:31:50 +0100

    updated index.js to adjust padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,29 +4,19 @@ import { Link } from "gatsby"
 const IndexPage = () => {
   return (
     <>
-      {/* <h1 className="font-bold text-2xl">Welcome to our store</h1>
-      <p className="p-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum.
-      </p> */}
-      <div className="container bg-lavender mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+      
+      <div className="container bg-lavender overflow-hidden mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
         {/* <!--Left Col--> */}
-        <div className="flex flex-col w-full lg:w-1/2 justify-center items-start pt-12 pb-24 px-6">
+        <div className="flex flex-col w-full lg:w-1/2 justify-center items-start pt-5 pb-24 px-6">
           <p className="uppercase tracking-loose">
             Your number 1 online store for luscious crunchy delicacies
           </p>
           <h1 className="font-bold text-3xl my-4">Crunchy Bread</h1>
           <p className="leading-normal mb-4">
-            Are you hungry and out of time? Are you looking for a quick way to satisfy your pizza cravings? Are you in search of the quickest way to get food without moving through the hassle of the traffic in your city? Not to worry. Crunchy Bread's got you covered! Order any luscious crunchy delicacy from the comfort of your home and have it delivered to you in no time. So what are you waiting for?
+            Are you hungry and out of time? Are you in search of the quickest way to get food without moving through the 
+    hassle of the traffic in your city? Not to worry. Crunchy Bread's got you covered!
+    Order any luscious crunchy delicacy from the comfort of your home and have it delivered to you in no time. 
+    So what are you waiting for?
           </p>
           <Link to="/products">
             <button className="bg-transparent hover:bg-purple-900 text-gray-900 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-gray-900 hover:border-transparent">
